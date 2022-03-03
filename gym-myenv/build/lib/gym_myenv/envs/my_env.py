c2582a799032e03a3600bc7e28eb7da507339294
Scott Garcia <scott.garcia40@t-mobile.com>
2022-03-02 16:57:22 -0800

    updated some typos

---
+++ gym-myenv/build/lib/gym_myenv/envs/my_env.py
@@ -0,0 +1,25 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class MyEnv(gym.Env):
+  metadata = {'render.modes': ['human', 'rgb_array']}
+
+  def __init__(self):
+    pass
+
+  def step(self, u):
+   pass   
+
+  def reset(self, **kwargs):
+    pass   
+
+  def render(self, mode='human'):
+    # pass   
+    if mode == 'human':
+      pass
+    elif mode == 'rgb_array':
+      pass
+			
+  def close(self):
+    pass
